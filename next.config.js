6d49acf86b986a052f862de80f3003c834d35c88
kurooooon <b1006110@gmail.com>
2019-12-04 22:46:29 +0900

    fix: env config

--- next.config.js
+++ next.config.js
@@ -2,9 +2,7 @@ require('dotenv').config();
 
 module.exports = {
   env: {
-    YOUTUBE_API_KEY: process.env.NODE_ENV === 'production'
-      ? YOUTUBE_API_KEY : process.env.YOUTUBE_API_KEY,
-    NEWS_SHEET_KEY: process.env.NODE_ENV === 'production'
-      ? NEWS_SHEET_KEY : process.env.NEWS_SHEET_KEY
+    YOUTUBE_API_KEY: process.env.YOUTUBE_API_KEY,
+    NEWS_SHEET_KEY: process.env.NEWS_SHEET_KEY
   }
 }
