4329438653c9f8e9b3ce94ed3ee4d531397aa4ef
kurooooon <b1006110@gmail.com>
2020-01-19 00:07:27 +0900

    add music api

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 module.exports = {
   env: {
     YOUTUBE_API_KEY: process.env.YOUTUBE_API_KEY,
-    NEWS_SHEET_KEY: process.env.NEWS_SHEET_KEY
+    NEWS_SHEET_KEY: process.env.NEWS_SHEET_KEY,
+    MUSIC_SHEET_KEY: process.env.MUSIC_SHEET_KEY
   }
 }
