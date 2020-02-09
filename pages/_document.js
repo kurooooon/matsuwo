466304ca243245b35c31fa89d5903036e487fb78
kurooooon <b1006110@gmail.com>
2020-02-09 23:25:57 +0900

    remove js

--- pages/_document.js
+++ pages/_document.js
@@ -286,12 +286,6 @@ class MyDocument extends Document {
           <noscript>
             <link rel="stylesheet" href="/static/assets/css/noscript.css" />
           </noscript>
-          <script src="assets/js/jquery.min.js"></script>
-          <script src="assets/js/jquery.scrolly.min.js"></script>
-          <script src="assets/js/browser.min.js"></script>
-          <script src="assets/js/breakpoints.min.js"></script>
-          <script src="assets/js/util.js"></script>
-          <script src="assets/js/main.js"></script>
         </Head>
         <Global styles={baseStyle} />
         <body>
